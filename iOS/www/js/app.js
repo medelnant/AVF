af75cc627f95e5f262d5670c33a9573889040a93
mEdelnant <medelnant@fullsail.edu>
2013-06-06 20:17:44 -0400

    Initial commit to add files to git repo

---
+++ iOS/www/js/app.js
@@ -0,0 +1,9 @@
+/* ##############################################
+Author: Michael Edelnant
+Course: AVF 1306 | Week 1
+Instructor: Jennifer McCarrick
+############################################## */
+
+var toBeDetermined = function() {
+	/* Make it rain */
+}
