d12db1ee64165df27f68047ca667cd25cc705602
mEdelnant <medelnant@fullsail.edu>
2013-06-20 22:20:19 -0400

    week3 modifications

--- iOS/www/js/app.js
+++ iOS/www/js/app.js
@@ -8,22 +8,45 @@ Instructor: Jennifer McCarrick
 // http://api.espn.com/v1/sports/basketball/nba/news/headlines?_accept=text/xml&limit=5&apikey=:yourkey
 
 //wait for device to signal ready
-// function onDeviceReady() {
-$(document).ready(function() {	
+
+
+var compassInit = '';
+var accelerometerInit = '';
+
+
+function onDeviceReady() {
+//$(document).ready(function() {
+	
+	/*########################################################
+	Connection API : Check status of network connection
+	########################################################*/
+	
+	//	var net = navigator.connection.type;
+	//	if(net == "none") {
+	//		$("#alertBanner").css("display","block");
+	//	}
+	
+	/*########################################################
+	End Connection API
+	########################################################*/	
 	
 	//home btn actions
 	$('#homeBtn').on('click', function(){
-		if($('.page').attr('id') === 'researchPage') {
-			$('.page').hide();
-		} else {
-			$('.page').remove();
-		}
+		
+		$('.page').remove();
+		$('#researchPage').hide();
 		
 		//Destroy Scrim
 		$('.scrim').remove();
 
 		//Reset Launch Page and remove blur
 		$('#launchPage').removeClass('inactive');
+
+		if(compassInit !== 0) {
+			navigator.compass.clearWatch(compassInit);
+		}
+
+
 	});
 	
 	
@@ -34,23 +57,21 @@ $(document).ready(function() {
 		switch($(this).attr("data-section")) {
 			
 			case "video":
+				setPageHeader('video','Vimeo Video');
+				$('' +
+					'<div id="videoPlaceHolder">' +
+						'<video controls preload="auto" poster="" data-setup="{}">' +
+							'<source src="https://dl.dropboxusercontent.com/u/146464/AVF/avf_week2.mp4" type="video/mp4">' +
+							'<source src="https://dl.dropboxusercontent.com/u/146464/AVF/avf_week2.webm" type="video/mp4">' +
+							'<source src="https://dl.dropboxusercontent.com/u/146464/AVF/avf_week2.ogv" type="video/mp4">' +
+						'</video>' +											
+					'</div>'
+				).appendTo($('#video'));
 				break;
 			
 			case "instagram":
-
-				// set class on launchPage to init blur 
-				$('#launchPage').addClass('inactive');
-				
-				// append masthead markup with page title
-				$('' +
-					'<div class="scrim"></div>' +
-					'<section class="page" id="instagram">' +
-						'<section class="appTitleMastHead">' +
-							'<h1><span>"SearsTower" Tagged Photos on Instagram</span></h1>' +
-						'</section>' +
-						'<ul class="gridContainer"></ul>' +                            
-					'</section>'
-				).appendTo($('#appWrapper'));
+				setPageHeader('instagram', 'Sears Tower Photos');
+				$('#instagram').append('<ul class="gridContainer"></ul>');
 
 				// call data api and build
 				$.ajax({
@@ -75,35 +96,23 @@ $(document).ready(function() {
 										'</div>' +
 									'</article>' +
 								'</li>'
-							).appendTo($('#instagram .gridContainer'));
-						});												
+							).appendTo($('.gridContainer'));
+						});											
 					}
 				});
 
 			break;
 			
 			case "espn":
-			  	
-			  	// set class on launchPage to init blur
-				$('#launchPage').addClass('inactive');
+				setPageHeader('espn', 'Sears Tower Photos');
+				$('#espn').append('<ul class="listContainer"></ul>');
 
-				// append masthead markup with page title
-				$('' +
-					'<div class="scrim"></div>' +
-					'<section class="page" id="espnNews">' +
-						'<section class="appTitleMastHead">' +
-							'<h1><span>ESPN Chicago Blackhawk News</span></h1>' +
-						'</section>' +
-						'<ul class="listContainer"></ul>' +                            
-					'</section>'
-				).appendTo($('#appWrapper'));
-				
 				// call data api and build
 				$.ajax({
 					url: "http://api.espn.com/v1/sports/hockey/nhl/teams/4/news",
 					data: {
-						apikey: 	"dk6z3rx49ru6breuud7wbnu4",
-						_accept: 	"application/json"
+						apikey:"dk6z3rx49ru6breuud7wbnu4",
+						_accept:"application/json"
 					},
 					dataType: "jsonp",
 					success: function(dataObj) {
@@ -125,19 +134,169 @@ $(document).ready(function() {
 										'</div>' +
 									'</article>' +
 								'</li>'
-							).appendTo($('#espnNews .listContainer'));
+							).appendTo($('.listContainer'));
 						});				
 					}
 				});	
 
 			break;
 			case "camera":
+
+				setPageHeader('camera', 'Camera');
+				$('' +
+						'<article>' +
+							'<div id="photoPlaceHolder">' +
+								'<img src="" />' +
+							'</div>' +							
+							'<div class="btn btn_fullWidth" id="cameraBtn">Take Photo</div>' +
+						'</article>' 
+				).appendTo($('#camera'));
+
+			
+
+
+
+				var cameraOptions = {
+					quality: 50, 
+					destinationType: Camera.DestinationType.FILE_URI				
+				};
+
+				$('#cameraBtn').on('click', function() {
+					navigator.camera.getPicture(onCameraSuccess, onCameraFail, cameraOptions);
+				});
+				 
+				function onCameraSuccess(imageURI) {
+					$('#photoPlaceHolder img')
+						.attr('src', imageURI)
+						.css('display', 'block');
+				}
+
+				function onCameraFail(message) {
+					alert('Failed because: ' + message);
+				}
+
+					
+
 			break;
 			case "accelerometer":
+
+				setPageHeader('accelerometer', 'Accelerometer');
+				$('' +
+						'<article>' +
+							'<ul class="formList">' +
+								'<li>' +
+									'<label>X:</label>' +
+									'<input type="text" id="accX" value="0" />' +
+								'</li>' +
+								'<li>' +
+									'<label>Y:</label>' +
+									'<input type="text" id="accY" value="0" />' +
+								'</li>' +
+								'<li>' +
+									'<label>Z:</label>' +
+									'<input type="text" id="accZ" value="0" />' +
+								'</li>' +								
+							'</ul>' +								
+						'</article>' 
+				).appendTo($('#accelerometer'));
+
+				var accelerometerOptions = {
+					frequency: 40
+				};
+				accelerometerInit = navigator.accelerometer.watchAcceleration(onAccelerometerSuccess, onAccelerometerError, accelerometerOptions );
+
+				function onAccelerometerSuccess(acceleration) {
+					$('#accX').attr('value', acceleration.x);
+					$('#accY').attr('value', acceleration.y);
+					$('#accZ').attr('value', acceleration.z);
+				} 
+
+				function onAccelerometerError(error) {
+					navigator.compass.clearWatch(accelerometerInit);
+					alert('Acceleration Not Found');
+				}									
+
 			break;
 			case "compass":
+				setPageHeader('compassPage', 'Compass');
+				$('' +
+						'<article>' +
+							'<div id="compass">' +
+								'<div id="compassPointer"></div>' +
+							'</div>' +														
+						'</article>'
+				).appendTo($('#compassPage'));
+
+				var compassOptions = {
+					frequency: 40
+				};
+				compassInit = navigator.compass.watchHeading(onCompassSuccess, onCompassError, compassOptions );
+
+				function onCompassSuccess(heading) {
+					var rotation = Math.round(heading.magneticHeading) + 'deg';
+					$('#compassPointer').css('-webkit-transform', 'rotate('+ rotation +')');
+				} 
+
+				function onCompassError(error) {
+					navigator.compass.clearWatch(compassInit);
+					alert('Compass Not Found');
+				}				
+						
+			
 			break;
 			case "geolocation":
+
+				setPageHeader('geolocation', 'Geolocation');
+				
+				$('' +
+						'<article>' +
+							'<ul class="formList">' +
+								'<li>' +
+									'<label>Lattitude:</label>' +
+									'<input type="text" id="geoLat" value="0" />' +
+								'</li>' +
+								'<li>' +
+									'<label>Longitude:</label>' +
+									'<input type="text" id="geoLong" value="0" />' +
+								'</li>' +
+							'</ul>' +								
+							'<div id="map"></div>' +
+						'</article>' 
+				).appendTo($('#geolocation'));	
+
+				navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
+				
+				function onGeoSuccess(position) {
+					
+					lattitude = position.coords.latitude;
+					longitude = position.coords.longitude;
+					
+					$('#geoLat').val(lattitude);
+					$('#geoLong').val(longitude);
+				
+					var newPosition = new google.maps.LatLng(lattitude,longitude);
+					
+					var mapoptions = {
+						zoom: 12,
+						center: newPosition,
+						mapTypeId: google.maps.MapTypeId.ROADMAP
+					};
+					
+					var map = new google.maps.Map(document.getElementById('map'), mapoptions);
+					
+				
+					var marker = new google.maps.Marker({
+							position: newPosition,
+							map: map
+					});
+				}
+				
+				function onGeoError(error) {
+					if( error == 1) {
+						alert('Turn on Geolocation services.');
+					}
+				}										
+
 			break;
 				
 			case "research":
@@ -148,12 +307,50 @@ $(document).ready(function() {
 				$('<div class="scrim"></div>').appendTo($('#appWrapper'));					
 				
 				//Toggle Show page
-				$('#researchPage').show();
-			break;		  		  		  		  		  		
+				$('.staticPage').show();
+			break;
 			default:
 			// code to be executed if n is different from case 1 and 2
 		}
 	});
+}
+
+
+$('#researchPage li a').on('click', function(){
+	$('.researchSection').hide();
+
+	switch($(this).attr("data-section")) {
+		case "week2":
+			$('#researchWeek2').show();
+		break;
+		case "week3":
+			$('#researchWeek3').show();
+		break;
+		case "week4":
+			$('#researchWeek4').show();
+		break;
+		default:	
+	}
+
 });
 
-// document.addEventListener("deviceready", onDeviceReady, false);
+
+var setPageHeader = function(section, title) {
+	// set class on launchPage to init blur 
+	$('#launchPage').addClass('inactive');
+	
+	// append masthead markup with page title
+	$('' +
+		'<div class="scrim"></div>' +
+		'<section class="page" id="'+ section +'">' +
+			'<section class="appTitleMastHead">' +
+				'<h1><span>'+ title +'</span></h1>' +
+			'</section>' +                          
+		'</section>'
+	).appendTo($('#appWrapper'));	
+};
+
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+
